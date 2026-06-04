7783d91b5ac3a302d7a0d251cb7e1272a0f343b6
imaninaitore <imaninaitore1726@gmail.com>
2026-06-04 19:09:41 +0300

    create div for dob in months then create js code for calculation and output

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ document.getElementById("greeting").innerHTML =
 
 });
 
+let dobInput = document.getElementById("dob");
+
+dobInput.addEventListener("change", function(){
+
+    let birthDate = new Date(dobInput.value);
+
+    let today = new Date();
+
+    let years = today.getFullYear() - birthDate.getFullYear();
+
+    let months = today.getMonth() - birthDate.getMonth();
+
+    let totalMonths = (years * 12) + months;
+
+    document.getElementById("months-result").innerHTML =
+    `You are approximately ${totalMonths} months old.`;
+
+});
